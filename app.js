ef7ec14cf510b3f0de89b25c8a072bd831cd67bf
IlyasAkhmerov <ilyas_akhmerov@mail.ru>
2026-08-14 12:05:42 +0300

    app.js updated

--- app.js
+++ app.js
@@ -315,7 +315,7 @@
         e.preventDefault();
         var user = usernameInput.value.trim();
         var pass = passwordInput.value.trim();
-        if (user === 'admin' && pass === '1234') {
+        if (user === 'auditor' && pass === '123456') {
             loginError.classList.add('hidden');
             isAuth = true;
             navigateTo('/dashboard');
@@ -358,7 +358,7 @@
         }
 
         console.log('✅ Сайт с чистыми URL запущен');
-        console.log('Логин: admin / 1234');
+        console.log('Логин: auditor / 123456');
         console.log('Доступные маршруты:');
         console.log('  /                      — главная');
         console.log('  /login                 — вход');
